7a9acfc6e9614b6a94b599acd47faa283d41b8c8
Brianna Doubt <bri@briannadoubt.com>
2022-09-26 16:51:26 -0700

    Add public initializer for deeper integrations

--- Sources/AlamofireEventSource/DecodableEventSourceMessage.swift
+++ Sources/AlamofireEventSource/DecodableEventSourceMessage.swift
@@ -8,8 +8,16 @@
 import Foundation
 
 public struct DecodableEventSourceMessage<T: Decodable> {
+    
     public var event: String?
     public var id: String?
     public var data: T?
     public var retry: String?
+    
+    public init(event: String? = nil, id: String? = nil, data: T? = nil, retry: String? = nil) {
+        self.event = event
+        self.id = id
+        self.data = data
+        self.retry = retry
+    }
 }
